e076cc856674999c579bbc5536f2c8bd1491fa3e
guilherme_gbm <guiwxz@hotmail.com>
2023-11-02 04:47:58 -0300

    feat: cria documentação com swagger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import '@/shared/config/connection';
+import swaggerDocs from '@/docs/swagger.json';
 
 import router from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/', router);
 
 app.listen(3000, () => {
