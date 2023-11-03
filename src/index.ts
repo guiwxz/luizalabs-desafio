de26bde3e7c9bc57d0a750e25a80e46725326348
guilherme_gbm <guiwxz@hotmail.com>
2023-11-03 05:57:26 -0300

    feat: add rota de health check

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@ import '@/shared/config/connection';
 import swaggerDocs from '@/docs/swagger.json';
 
 import { app } from './app';
+import mongoose from 'mongoose';
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get('/healthcheck', async (_req, res) => {
+  const isDatabaseRunning = mongoose.connection.readyState === 1;
+
+  return res.json({ database: isDatabaseRunning ? 'up' : 'down', api: 'up' });
+});
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000');
